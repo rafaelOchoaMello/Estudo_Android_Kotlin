1bc779df662cb8e1c1b157e7c064b6a170f04d4c
rafaelochoamello <rafaelochoamello@gmail.com>
2022-01-31 20:57:14 -0300

    Update RecyclerView to dinamic content

--- app/src/main/java/com/curso/primeiroapp/RecyclerViewActivity.kt
+++ app/src/main/java/com/curso/primeiroapp/RecyclerViewActivity.kt
@@ -4,19 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.curso.primeiroapp.model.Jogos
 import com.curso.primeiroapp.recyclerViewAdapter.RecyclerViewAdapter
 
 class RecyclerViewActivity : AppCompatActivity() {
+
+    private val jogos: ArrayList<Jogos> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
 
+        //Carrega os filmes que serão passados para o nosso RecyclerView
+        loadJogos()
+
         //RECYCLERVIEW ::::
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
 
         //Define e configura o Adapter com base na nossa classe 'RecyclerViewAdapter'
-        val myAdapter: RecyclerViewAdapter = RecyclerViewAdapter()
+        //É na nossa classe que vamos passar a lista de jogos
+        val myAdapter: RecyclerViewAdapter = RecyclerViewAdapter(jogos)
 
 
         //CONFIGURAÇÕES RECYCLERVIEW ::::
@@ -27,5 +34,13 @@ class RecyclerViewActivity : AppCompatActivity() {
         //Define o adapter
         recyclerView.adapter = myAdapter
 
+    }
+
+    private fun loadJogos(){
+        this.jogos.add(Jogos("Super Mario Bros", "Plataforma", "1985"))
+        this.jogos.add(Jogos("Super Mario Bros 2", "Plataforma", "1986"))
+        this.jogos.add(Jogos("Super Mario Bros", "Plataforma", "1988"))
+        this.jogos.add(Jogos("Pokémon Yellow", "RPG", "1998"))
+        this.jogos.add(Jogos("Pokémon Gold", "RPG", "1999"))
     }
 }
